c57d5d665dc982e4b7f2fded97ec3069db4f7496
Kareem Janoudi <kjanoudi@gmail.com>
2015-12-04 01:49:40 -0500

    added devise installer

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Rails4TwitterClone
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
